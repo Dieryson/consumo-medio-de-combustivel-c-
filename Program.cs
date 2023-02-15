59e8b951a7778fa410eb32b0e88b0b738df31d1b
Dieryson Araujo <115048301+Dieryson@users.noreply.github.com>
2023-02-15 12:59:39 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ConsumoMedioDeCombustivel
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int distancia;
+            double combustivelGasto, consumoMedio;
+
+            Console.WriteLine("Informe a distancia percorrida");
+
+            distancia = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe quantos litros de combustivel gastou nesta viagem ");
+            combustivelGasto = Convert.ToDouble(Console.ReadLine());
+
+            consumoMedio = distancia / combustivelGasto; //atribua a formula para que o codigo funcione corretamente
+
+            Console.WriteLine("Distancia media para cada litro de combustivel gasto foi de  {0:0.000} km/l", consumoMedio);
+
+        }
+    }
+}
+
+
